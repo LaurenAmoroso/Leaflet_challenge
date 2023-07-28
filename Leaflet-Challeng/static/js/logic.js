3512e07ae1436be9a752e80c5e5d5cbeaf67b96f
LaurenAmoroso <129470677+LaurenAmoroso@users.noreply.github.com>
2023-07-27 18:15:56 -0700

    Add files via upload

---
+++ Leaflet-Challeng/static/js/logic.js
@@ -0,0 +1,88 @@
+// Store API
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// get request
+d3.json(url).then(function (data) {
+  // Console log the data retrieved 
+  console.log(data);
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+// Function to determine marker size
+function markerSize(magnitude) {
+  return magnitude * 2000;
+};
+
+// Function to determine marker color by depth
+function chooseColor(depth){
+  if (depth < 10) return "#00FF00";
+  else if (depth < 30) return "greenyellow";
+  else if (depth < 50) return "yellow";
+  else if (depth < 70) return "orange";
+  else if (depth < 90) return "orangered";
+  else return "#FF0000";
+}
+
+function createFeatures(earthquake_data) {
+
+  // Give each feature a popup for place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquake_data object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  var earthquakes = L.geoJSON(earthquake_data, {
+    onEachFeature: onEachFeature,
+
+    // Layer used to alter markers
+    pointToLayer: function(feature, latlng) {
+
+      // Determine the style of markers based on properties
+      var markers = {
+        radius: markerSize(feature.properties.mag),
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
+        fillOpacity: 0.7,
+        color: "black",
+        stroke: true,
+        weight: 0.5
+      }
+      return L.circle(latlng,markers);
+    }
+  });
+
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Create tile layer
+  var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+  // Create our map
+  var USA_Map = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 10,
+    layers: [basemap, earthquakes]
+  });
+
+  // Add legend
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+
+    for (var i = 0; i < depth.length; i++) {
+      div.innerHTML +=
+      '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+  legend.addTo(USA_Map)
+};
